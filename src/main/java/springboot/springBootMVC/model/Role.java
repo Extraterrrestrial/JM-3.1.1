d3b294ea37a45f85edf4939e267c010151e1a9ad
Yuri Kriger <yuri.kriger@gmail.com>
2021-11-17 04:28:47 +0600

    Commit

---
+++ src/main/java/springboot/springBootMVC/model/Role.java
@@ -0,0 +1,30 @@
+package springboot.springBootMVC.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import javax.persistence.*;
+import java.util.Set;
+/**
+ * C @Data падает с stackoverflow
+ */
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role")
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
+
+
